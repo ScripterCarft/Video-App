7f798f37a070fbe339639a9210cc82bbfd8e76fe
Joe <146555209+ScripterCarft@users.noreply.github.com>
2026-09-20 19:05:49 +0200

    Tabbar working

--- App.tsx
+++ App.tsx
@@ -1,45 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeBottomTabNavigator } from '@react-navigation/bottom-tabs/unstable';
+import HomeScreen from './src/screens/HomeScreen.tsx';
+import FollowingScreen from './src/screens/FollowingScreen.tsx';
+import LibraryScreen from './src/screens/LibraryScreen.tsx';
+import SearchScreen from './src/screens/SearchScreen.tsx';
+import { homeIcon, followingIcon, libraryIcon, searchIcon } from './src/navigation/TabIcons';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const Tab = createNativeBottomTabNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ tabBarIcon: homeIcon }}
+        />
+        <Tab.Screen
+          name="Following"
+          component={FollowingScreen}
+          options={{ tabBarIcon: followingIcon }}
+        />
+        <Tab.Screen
+          name="Library"
+          component={LibraryScreen}
+          options={{ tabBarIcon: libraryIcon }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ tabBarIcon: searchIcon }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ src/screens/LibraryScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function LibraryScreen() {
+  return (
+    <View>
+      <Text>Library</Text>
+    </View>
+  )
+}
+
+export default LibraryScreen;
